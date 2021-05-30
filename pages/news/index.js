47623b88080d8a3dc8008df0ca3e380a555d3356
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-30 12:21:12 -0500

    link from next

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,22 @@
 //localhost:3000/news
 
+import { Fragment } from "react";
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/nextjs">NextJS Framework</Link>
+        </li>
+        <li>
+          <Link href="/reactjs">NextJS Framework</Link>ReactJS Framework
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
