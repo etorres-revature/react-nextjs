929a9139773e083343a8015c52da900d3263ebf8
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-30 13:02:09 -0500

    meetup detail comp and styling

---
+++ pages/[meetupID]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeeupDetail";
+
+const MeetUpDetail = () => {
+  return (
+    <MeetupDetail
+      image="https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/10/Hand-Luggage-Only-2-1.jpg?w=1600&ssl=1"
+      title="first meetup"
+      address="Some street, your city"
+      description="this is the first meetup"
+    />
+  );
+};
+
+export default MeetUpDetail;
