d526769d21148aadb1ab7e824915a186f4074799
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-30 12:14:06 -0500

    use Router custom hook for url params

--- pages/news/[newsID].js
+++ pages/news/[newsID].js
@@ -1,6 +1,14 @@
 //localhost:3000/news/**
+import { useRouter } from "next/router";
 
 const ImportantPage = () => {
+  const router = useRouter();
+
+  const newsID = router.query.newsID;
+
+  //send request to backend API
+  //to fetch the news id with newsID variable
+
   return <h1>The News - something important happened</h1>;
 };
 
