6136de6848e4314e4d3b047a34e7e237a83e65de
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-30 14:28:17 -0500

    set fallback on single meet up page to blocking

--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
   (await client).close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meeetups.map((meetup) => ({
       params: { meetupID: meetup._id.toString() },
     })),
